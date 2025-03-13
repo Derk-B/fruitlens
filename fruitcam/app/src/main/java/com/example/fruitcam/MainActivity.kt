4135312a86257341a3976a205205aff8ead0a02c
derk <derk@desktop-derk.home>
2025-03-13 23:21:12 +0100

    Added camera app with basic layout

---
+++ fruitcam/app/src/main/java/com/example/fruitcam/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.fruitcam
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.fruitcam.ui.theme.FruitcamTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CameraPreview()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun CameraPreview() {
+    FruitcamTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            CameraPreviewScreen()
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .padding(bottom = 50.dp),
+                verticalArrangement = Arrangement.Bottom
+
+            ) {
+                var isProcessing by remember { mutableStateOf(false) }
+                if (!isProcessing) {
+                    Button(
+                        onClick = { isProcessing = true },
+                        shape = CircleShape,
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        modifier = Modifier
+                            .size(width = 60.dp, height = 60.dp)
+                            .align(Alignment.CenterHorizontally),
+                    ) {}
+                } else {
+                    CircularProgressIndicator(
+                        color = Color.White,
+                        modifier = Modifier
+                            .size(width = 60.dp, height = 60.dp)
+                            .align(
+                                Alignment.CenterHorizontally
+                            ),
+                    )
+                }
+            }
+        }
+    }
+}
